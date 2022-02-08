1bdcbaf5170f5d3a15c88081e86adb7af63fda5d
Aleksa Cekic <aleksa.cekic8@gmail.com>
2022-02-09 00:58:37 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,66 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Main {
+    public static void main(String[] args) {
+//        Scanner userInput = new Scanner(System.in);
+        String excelFilePath = "eli.xlsx";
+//        String excelFilePath = userInput.nextLine();
+
+        try {
+            FileInputStream inputStream = new FileInputStream(excelFilePath);
+            XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+
+            for (int i = 3; i < sheet.getPhysicalNumberOfRows(); i++) {
+                XSSFRow row = sheet.getRow(i);
+                String cdl;
+                if (row.getCell(1).getCellType() == CellType.STRING) {
+                    cdl = row.getCell(1).getStringCellValue();
+                } else {
+                    cdl = String.valueOf(row.getCell(1).getNumericCellValue());
+                }
+                String issueProvince = row.getCell(2).getStringCellValue();
+
+                if (issueProvince.equals("ON")) {
+                    cdl = fixCdl(cdl);
+                    XSSFCell cell = row.getCell(1);
+                    cell.setCellValue(cdl);
+                }
+            }
+
+            inputStream.close();
+
+            FileOutputStream outputStream = new FileOutputStream(excelFilePath);
+
+            workbook.write(outputStream);
+            workbook.close();
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String fixCdl(String cdl) {
+        StringBuilder builder = new StringBuilder();
+        char[] arr = cdl.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            builder.append(arr[i]);
+            if ((i + 1) % 5 == 0 && i != arr.length - 1) {
+                builder.append("-");
+            }
+        }
+
+        return builder.toString();
+    }
+}
