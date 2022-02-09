df892f2d825d2fd5c41c6ac048d88e1c4f5d78c2
Aleksa Cekic <aleksa.cekic8@gmail.com>
2022-02-09 01:43:21 +0100

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# java-excel-template
+## To run this project you will need **JDK**
+
+To download and install latest version of JDK go to: [Oracle](https://www.oracle.com/java/technologies/downloads/)
+
+### After you finished with installing JDK, you will need to do:
+
+1. Install all of the apache poi dependencies that have been used,
+2. Put your excel file that has the exact same tamplate to `java-excel-template` project.
+3. Run the project
+
+### Your excel data should change after finish!
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Scanner;
 
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.xssf.usermodel.XSSFCell;
@@ -12,9 +10,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Main {
     public static void main(String[] args) {
-//        Scanner userInput = new Scanner(System.in);
         String excelFilePath = "eli.xlsx";
-//        String excelFilePath = userInput.nextLine();
 
         try {
             FileInputStream inputStream = new FileInputStream(excelFilePath);
